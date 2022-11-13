fcad8bfc01b2695d7034f7ac2d2a1b06883ffe55
João Novo <jcnew16@gmail.com>
2022-11-13 12:32:59 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function openSocial() {
+    let sw = document.getElementById("switch")
+    let menu = document.getElementById("menu")
+    sw.classList.toggle("active");
+    menu.classList.toggle("active");
+}
